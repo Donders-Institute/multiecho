cda203a0d28922f323a7422a7796ed2da4aaaf33
MarsMellow <mpzwiers@gmail.com>
2021-03-31 08:38:00 +0200

    Bump version to 0.26

--- setup.py
+++ setup.py
@@ -37,4 +37,6 @@ setup(name                          = 'multiecho',          # Required
                                        'Operating System :: OS Independent',
                                        'Programming Language :: Python :: 3.6',
                                        'Programming Language :: Python :: 3.7',
+                                       'Programming Language :: Python :: 3.8',
+                                       'Programming Language :: Python :: 3.9',
                                        'Programming Language :: Python :: Implementation :: CPython'])
